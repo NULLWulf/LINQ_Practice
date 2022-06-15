088a5bd30f7b3da407272cfee84e254eabd2d6ad
Nathan <89611095+NULLWulf@users.noreply.github.com>
2022-06-15 13:30:40 +0000

    ~~

--- main.cs
+++ main.cs
@@ -1,11 +1,24 @@
 using System;
 using System.Linq;
 
+class Student {
+
+  public Student(int studentId, string studentName, int age){
+    StudentID = studentId;
+    StudentName = studentName;
+    Age = age;
+  }
+  
+  public int StudentID {get; set; }
+  public string StudentName { get; set; }
+  public int Age { get; set; }
+}
+
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
 
-    firstLinq();
+    // firstLinq();
   }
 
   public static void firstLinq(){
@@ -17,5 +30,31 @@ class Program {
 
     foreach(var name in myLinqQuery)
       Console.Write(name + " ");
+  }
+
+  public static void studentRun(){
+    Student[] studentArray = {
+      new Student(1,"John",18),
+      new Student(2,"Steve",21),
+      new Student(3,"Bill",25),
+      new Student(4,"Ram",20),
+      new Student(5,"Ron",31),
+      new Student(6,"Chris",17),
+      new Student(7,"Rob",19)
+    };
+
+    // Student[] students = new Student[10];
+
+    // int i = 0;
+
+    // foreach (Student std in studentArray){
+      
+    //   if (std.Age > 12 && std.Age < 20){
+    //     students[i] = std;
+    //     i++;
+    //   }
+    // }
+
+    Student[] teenAgerStudents = studentArray.Where(s => s.Age > 12 && s.Age < 20).ToArray();  
   }
 }
