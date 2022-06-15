1f15f3241514bc83568f492101d4adcddd6c3e76
Nathan <89611095+NULLWulf@users.noreply.github.com>
2022-06-15 12:56:36 +0000

    Added first linq function

--- main.cs
+++ main.cs
@@ -1,7 +1,21 @@
 using System;
+using System.Linq;
 
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+
+    firstLinq();
+  }
+
+  public static void firstLinq(){
+    string[] names = {"Bills","Steve","James","Mohan"};
+
+    var myLinqQuery = from name in names
+                    where name.Contains('a')
+                    select name;
+
+    foreach(var name in myLinqQuery)
+      Console.Write(name + " ");
   }
 }
